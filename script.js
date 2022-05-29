56f18817855612d91f7541a60750240e6254bb87
Felipe <fe3ky07@gmail.com>
2022-05-29 20:52:49 -0300

    Dia 2

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const dollar = {
-    id:"6ZLPFMAYRN282",
-    type:"USD",
-    currency:"$",
-    values: ['5,00','10,00','15,00','20,00','25,00'],
-}
+  id: "6ZLPFMAYRN282",
+  type: "USD",
+  currency: "$",
+  values: ["5,00", "10,00", "15,00", "20,00", "25,00"],
+};
 
 const euro = {
-    id:"HY4PAF5W6XZ6Y",
-    type:"EUR",
-    currency:"€",
-    values: ['4,00','8,00','12,00','16,00','20,00'],
-}
+  id: "HY4PAF5W6XZ6Y",
+  type: "EUR",
+  currency: "€",
+  values: ["4,00", "8,00", "12,00", "16,00", "20,00"],
+};
 
 const change_currency = (moeda) => {
-    const buttonID = document.getElementsByName("hosted_button_id")[0];
-    buttonID.setAttribute("value", moeda.id);
-    const select = document.getElementsByName("os0")[0];
-    const options = select.getElementsByTagName("option")
-}
+  const buttonID = document.getElementsByName("hosted_button_id")[0];
+  buttonID.setAttribute("value", moeda.id);
+  const select = document.getElementsByName("os0")[0];
+  const options = select.getElementsByTagName("option");
+  for (var i = 0; i < options.length; i++) {
+    options[
+      i
+    ].textContent = `${moeda.currency}${moeda.values[i]} ${moeda.type}`;
+  }
+};
+
+const select_moeda = () => {
+  const select = document.getElementById("os2").value;
+  change_currency(select == "dollar" ? dollar : euro);
+};
+
+change_currency(dollar);
